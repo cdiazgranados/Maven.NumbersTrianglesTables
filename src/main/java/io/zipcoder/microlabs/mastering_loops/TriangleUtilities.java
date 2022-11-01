1fb48362182c4e968646d8931213547b8682d996
Carolina Diazgranados <linacello@gmail.com>
2022-10-31 22:00:13 -0400

    Created triangles using nested for loops in TriangleUtilities. Tests have not passed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,64 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfRows; j++) {
+                if (j > i) {
+                    triangle = triangle + "\n";
+                    break;
+                } else {
+                    triangle = triangle + "*";
+                }
+            }
+        }
+        System.out.println(triangle);
+
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        for (int i = 0; i < 20; i++) {
+            row += "*";
+        }
+        return row;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (j > i) {
+                    triangle = triangle + "\n";
+                    break;
+                } else {
+                    triangle = triangle + "*";
+                }
+            }
+        }
+        System.out.println(triangle);
+
+        return triangle;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (j > i) {
+                    triangle = triangle + "\n";
+                    break;
+                } else {
+                    triangle = triangle + "*";
+                }
+            }
+        }
+        System.out.println(triangle);
+
+        return triangle;
     }
 }
